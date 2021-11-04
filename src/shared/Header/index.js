1a280a6dc4151eeefeafa2d1c869bc843035520f
bonino97 <juanbonino97@gmail.com>
2021-11-03 21:59:18 -0300

    Improving header

--- src/shared/Header/index.js
+++ src/shared/Header/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-primary justify-content-between'>
       <div className='container'>
-        <h1>CALDAR</h1>
+        <h1>
+          <Link to={'/'} className='text-light'>
+            CALDAR
+          </Link>
+        </h1>
       </div>
 
-      <button className='btn btn-danger nuevo-post d-block d-md-inline-block'>
-        Agregar Edificio &#43;
-      </button>
+      <Link
+        to={'/buildings/new'}
+        className='btn btn-danger nuevo-post d-block d-md-inline-block'
+      >
+        Nuevo Edificio &#43;
+      </Link>
     </nav>
   );
 };
