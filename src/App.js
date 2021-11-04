475fee68b659d163bb60b5f9cdea0e8f7d69b62f
bonino97 <juanbonino97@gmail.com>
2021-11-03 21:59:23 -0300

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './shared/Header';
+import Buildings from './components/Buildings';
+import NewBuilding from './components/Buildings/NewBuilding';
+import EditBuilding from './components/Buildings/EditBuilding';
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <div className='container'>
+        <Switch>
+          <Route exact path='/' component={Buildings} />
+          <Route exact path='/buildings/new' component={NewBuilding} />
+          <Route exact path='/buildings/edit/:id' component={EditBuilding} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
