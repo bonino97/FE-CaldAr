6443b70ad66170c2bc49ba5a8a979d801e2d3ab3
rubensato <rubensato@gmail.com>
2021-12-01 18:13:49 -0300

    boiler item component with edit and delete functionalities

--- src/components/Boilers/Boiler/index.js
+++ src/components/Boilers/Boiler/index.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
+import {
+  deleteBoilerAction,
+  setBoilerAction,
+} from '../../../store/actions/boilersActions';
 
 const Boiler = ({ boiler }) => {
-  const { _id, description, type } = boiler;
+  const { _id, name, email, phone } = boiler;
 
   const dispatch = useDispatch();
   const history = useHistory(); // Habilitar history para redireccionar.
 
+  const onDeleteBoiler = (id) => {
+    // preguntar al usuario
+    Swal.fire({
+      title: '¿Estas seguro?',
+      text: 'Esta accion es irreversible.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteBoilerAction(id));
+      }
+    });
+  };
+
+  const onEditRedirection = (id) => {
+    dispatch(setBoilerAction(boiler));
+    history.push(`/boilers/edit/${id}`);
+  };
+
   return (
     <tr>
       <td>
-        <span className='font-weight-bold'> {_id} </span>
+        <span className='font-weight-bold'> {name} </span>
       </td>
-      <td>{description}</td>
-      <td>{type}</td>
+      <td>{email}</td>
+      <td>{phone}</td>
       <td className='actions'>
         <button
           type='button'
-          onClick=''
+          onClick={() => onEditRedirection(_id)}
           className='btn btn-primary m-1'
         >
           Editar
@@ -27,7 +55,7 @@ const Boiler = ({ boiler }) => {
         <button
           type='button'
           className='btn btn-danger m-1'
-          onClick=''
+          onClick={() => onDeleteBoiler(_id)}
         >
           Eliminar
         </button>
