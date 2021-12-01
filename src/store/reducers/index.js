e2e60d0683a4fce9a178b085a24448aafcfd97c0
rubensato <rubensato@gmail.com>
2021-12-01 17:04:01 -0300

    Redux: combine boilers reducer file in default reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import buildingsReducer from './buildingsReducer';
+import boilersReducer from './boilersReducer';
 
 // Se utiliza un reducer Index que integrara todos los demas reducers.
 
 export default combineReducers({
   buildings: buildingsReducer,
+  boilers: boilersReducer,
 });
