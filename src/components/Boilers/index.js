73de970bdd4b678586c7f880c16bfaa27bade4c4
rubensato <rubensato@gmail.com>
2021-12-01 17:36:06 -0300

    boilers list with fetching data mapping applying redux thunk

--- src/components/Boilers/index.js
+++ src/components/Boilers/index.js
@@ -1,94 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Boiler from './Boiler';
 
+import { getAllBoilersAction } from '../../store/actions/boilersActions';
+import { addNewBoilerAction } from '../../store/actions/boilersActions';
+
+
 const Boilers = () => {
-  const [boilers, setBoilers] = useState([]);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const getBoilers = async () => {
-      const boilersFromServer = await fetchBoilers();
-      setBoilers(boilersFromServer);
-    };
-
-    getBoilers();
+    const getAllBoilers = () => dispatch(getAllBoilersAction());
+    getAllBoilers();
   }, []);
 
-  //fetch boilers (All)
-  const fetchBoilers = async () => {
-    const res = await fetch('http://localhost:5000/boilers');
-    const data = await res.json();
+  const { loading, error, boilers } = useSelector((state) => state.boilers);
 
-    return data;
-  };
+  // Llama el action.
+  const addNewBoiler = (boiler) => dispatch(addNewBoilerAction(boiler));
 
-  //fetch a boiler (by id)
-  // const fetchBoiler = async (id) => {
-  //   const res = await fetch(`http://localhost:5000/boilers/{id}`);
-  //   const data = await res.json();
-
-  //   return data;
-  // };
-
-  //Add Boiler
-  const addBoiler = async (boiler) => {
-    const res = await fetch(`http://localhost:5000/boilers`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(boiler),
-    });
-
-    const data = await res.json();
-
-    setBoilers([...boilers, data]);
-  };
-
-  // Delete Boiler
-  const deleteBoiler = async (id) => {
-    await fetch(`http://localhost:5000/boilers/${id}`, {
-      method: 'DELETE',
-    });
-
-    setBoilers(boilers.filter((boiler) => boiler.id !== id));
-  };
 
   return (
-    <>
-      <div className='row col-10 my-5 justify-content-between'>
-        <h2 className=''>Listado de calderas</h2>
-        <Link
-          to={'/boilers/new'}
-          className='btn btn-danger nuevo-post d-block d-md-inline-block'
-        >
-          Nueva Caldera &#43;
-        </Link>
-      </div>
-      <table className='table table-striped'>
-        <thead className='bg-primary table-dark'>
-          <tr>
-            <th scope='col'> Id &#35; </th>
-            <th scope='col'> Descripcion </th>
-            <th scope='col'> Tipo </th>
-            <th scope='col'> Acciones </th>
-          </tr>
-        </thead>
-        <tbody>
-          {boilers.length === 0
-            ? 'No hay calderas registradas para mostrar'
-            : boilers.map((boiler) => (
-                <Boiler
-                  key={boiler.id}
-                  boiler={boiler}
-                  onDelete={deleteBoiler}
-                />
-              ))}
-        </tbody>
-      </table>
-    </>
+    <div>
+      <>
+        <h2 className='text-center my-5'>Listado de Proveedores</h2>
+        {loading ? <h4 className='text-center'> Loading... </h4> : null}
+
+        {error ? (
+          <p className='alert alert-danger p-2 m-4 text-center'>
+            Ocurrio un error.
+          </p>
+        ) : null}
+
+        <div className='row pb-2'>
+          <div className='col-12 text-center'>
+            <button className='btn btn-primary m-1' onClick=''>
+              Agregar Proveedor (React Forms)
+            </button>
+            <button className='btn btn-primary m-1' onClick=''>
+              Agregar Proveedor (Final Forms)
+            </button>
+          </div>
+        </div>
+
+        <table className='table table-striped'>
+          <thead className='bg-primary table-dark'>
+            <tr>
+              <th scope='col'>Nombre</th>
+              <th scope='col'>Email</th>
+              <th scope='col'>Tel&eacute;fono</th>
+              <th scope='col'>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {boilers.length === 0
+              ? 'No hay Proveedores para mostrar'
+              : boilers.map((boiler) => (
+                  <Boiler key={boiler._id} boiler={boiler} />
+                ))}
+          </tbody>
+        </table>
+      </>
+    </div>
   );
 };
 
 export default Boilers;
+
+
+
